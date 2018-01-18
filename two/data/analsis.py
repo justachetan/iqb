ae439f590f950f159de4f3c7453c4426ea807581
adsrc <aditya16217@iiitd.ac.in>
2018-01-18 15:11:11 +0530

    Started with Assignment 2

---
+++ two/data/analsis.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_cell_magic('html', '', '<style>\ntable {float:left}\n</style>')
+
+
+# BIO215 - Introduction to Quantitative Biology
+# =========================
+# 
+# 
+# 
+# ## Homework Assignment 2
+# 
+# | Name | Aditya Chetan |          
+# | ------------- |-------------| 
+# | Roll No.     | 2016217 |
+# 
+# 
+
+# ### Part I
+# 
+# 
+
+# ### Part II
+# 
+# 
